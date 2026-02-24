f724a5c7396f0e27fe15fce1412eb06d36dd34a8
Deployment Agent <test@example.com>
2026-02-25 01:29:44 +0530

    Restore User.fromJson method that was accidentally removed

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -11,6 +11,12 @@ class User {
     required this.avatar,
   });
 
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'] ?? json['_id'] ?? '',
+      username: json['username'] ?? '',
+      email: json['email'] ?? '',
+      avatar: json['avatar'] ?? '',
     );
   }
 
